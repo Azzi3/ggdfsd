59023acdcbf1a1d9c60985e4bd31e217f69af3d1
Timalex <tim.alexander.gunnarsson@gmail.com>
2015-01-29 10:51:32 +0100

    Some company labels

--- foretagsprofil.php
+++ foretagsprofil.php
@@ -1,1 +1,25 @@
-<?php echo '<p>Hello Mother Duckers</p>'; ?> 
+<html>
+<head>
+
+</head>
+<body>
+<form action="">
+    <fieldset>
+        <label>Namn <input type="text"/></label>
+        <fieldset>
+            <legend>Företagsadress</legend>
+            <label>Gatuadress</label>
+            <label></label>
+        </fieldset>
+        <fieldset>
+            <legend>Kontaktuppgifter</legend>
+
+        </fieldset>
+        <label>Webbadress</label>
+        <label>Företagsbeskrivning</label>
+        <label>Tekniker</label>
+    </fieldset>
+</form>
+</body>
+</html>
+<?php echo '<p>Hello Mother Duckers</p>'; ?>
