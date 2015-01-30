bd6daee0538cfa0d3404ea136d51cf3f17a76cb7
CrazyDave1 <info@davidahlander.se>
2015-01-30 10:34:32 +0100

    Fixed some fields

--- public/views/foretagsprofil.php
+++ public/views/foretagsprofil.php
@@ -16,23 +16,36 @@
 
     <?php require_once '../../partials/project-header.php'; ?>
 
-    <h1>Registrera ert företag</h1>
+    <h1>Hantera företagsprofil</h1>
 
     <div class="table-responsive">
 
         <div class="container">
             <form>
-                <div class="form-group">
-                    <label for="companyPasswordNew">Nytt lösenord</label>
+                <!-- <div class="form-group">
+                    <label for="companyChangedPassword">Ändra lösenord</label>
                     <input type="text" class="form-control" id="companyPassword" placeholder="Skriv nytt lösenord">
                 </div>
                 <div class="form-group">
-                    <label for="companyPasswordNewRepeted">Repeterat lösenord</label>
-                    <input type="text" class="form-control" id="companyComfirmdPassword" placeholder="Repetera valt lösenord">
+                    <label for="companyConfirmPassword">Bekräfta lösenord</label>
+                    <input type="text" class="form-control" id="companyComfirmdPassword" placeholder="Bekräfta lösenord">
+                </div> -->
+
+                <legend>Kontaktperson</legend>
+                <div class="form-group">
+                    <label for="contactPersonName">Namn:</label>
+                    <input type="text" class="form-control" id="contactPersonName" placeholder="Kontaktperson">
+                </div>
+                <div class="form-group">
+                    <label for="contactPersonTel">Telefonnummer:</label>
+                    <input type="tel" class="form-control" id="contactPersonTel" placeholder="Kontaktpersons telefonnummer">
+                </div>
+                <div class="form-group">
+                    <label for="contactPersonEmail">E-post:</label>
+                    <input type="email" class="form-control" id="contactPersonEmail" placeholder="Kontaktpersons e-post">
                 </div>
 
-
-
+                <legend>Företagsinfo</legend>
                 <div class="form-group">
                     <label for="companyName">Företagsnamn</label>
                     <input type="text" class="form-control" id="companyName" placeholder="Företagsnamn">
@@ -50,43 +63,25 @@
                     <input type="text" class="form-control" id="companyCity" placeholder="Postort">
                 </div>
 
-
-                    <legend>Kontaktperson</legend>
-                <div class="form-group">
-                    <label for="contactPersonName">Namn:</label>
-                    <input type="text" class="form-control" id="contactPersonName" placeholder="Kontaktperson">
-                </div>
-                <div class="form-group">
-                    <label for="contactPersonTel">Telefonnummer:</label>
-                    <input type="tel" class="form-control" id="contactPersonTel" placeholder="Kontaktpersons telefonnummer">
-                </div>
-                <div class="form-group">
-                    <label for="contactPersonEmail">E-post:</label>
-                    <input type="email" class="form-control" id="contactPersonEmail" placeholder="Kontaktpersons e-post">
-                </div>
-
-                    <legend>Företagsinfo</legend>
                 <div class="form-group">
                     <label for="companyURL">Webbadress:</label>
                     <input type="url" class="form-control" id="companyURL" placeholder="Webbadress">
                 </div>
                 <div class="form-group">
                     <label for="companyDescription">Företagsbeskrivning:</label>
-                    <input type="text" class="form-control" id="companyDescription" placeholder="Företagsbeskrivning">
+                    <textarea id="companyDescription" class="form-control" rows="3"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="companyTags">Företagstaggar</label>
-                    <select class="form-control" id="companyTags">
-                        <option value="java">JAVA</option>
-                        <option value="php">PHP</option>
-                        <option value="mysql">MySQL</option>
-                        <option value="css">CSS</option>
-                    </select>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="tags" value="JAVA">JAVA
+                    </label>
+                </div>
+                    
                 </div>
 
-
-            
-                <button type="submit" class="btn btn-default">Submit</button>
+                <button type="submit" class="btn btn-default">Spara</button>
             </form>
         </div>
     </div>
