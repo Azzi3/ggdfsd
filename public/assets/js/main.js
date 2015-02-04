a39cd1dab65a8dbec308fbeafa4049f4662c57c3
Bachstatter <joachim.bachstatter@gmail.com>
2015-02-04 14:14:00 +0100

    Delete is working like a boss

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,7 +2,7 @@
 $(document).on('click', '#deleteBtn', function (e) {
 	var id = $(this).data('projectid');
 	$(document).on('click', '#reallyDelete', function (e) {
-		var url = location.origin + location.pathname + '?id='+ id;
+		var url = location.origin + location.pathname + '?deleteid='+ id;
 		location.href = url;
 	});
 });
