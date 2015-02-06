1e6a0fc70bc8d5999b8c13ca10246af8b9da8518
Bachstatter <joachim.bachstatter@gmail.com>
2015-02-06 14:24:08 +0100

    backend validation working, no filler

--- views/manage-projects.php
+++ views/manage-projects.php
@@ -28,27 +28,65 @@ if(isset($_GET['id'])){
 //insert function
 if(isset($_POST['project'])){
     $project = $_POST['project'];
+    $form = $_POST['project'];
+    $session->setSession('form',$project);
+    $error = array();
+    if(strlen($form['name']) < 2 || strlen($form['name']) > 40){
+        $error[] .= 'Namnet måste vara mellan 2 och 40 tecken.';
+    }
+    if(strlen($form['description']) < 2 || strlen($form['description']) > 300){
+        $error[] .= 'Beskrivingen måste mellan 2 och 300 tecken.';
+    }
+    if(!is_numeric($form['spots']) && $form['spots']  < 1){
+        $error[] .= 'Platser måste vara ett nummer och större än 0.';
+    }
+    if(strlen($form['company']) < 2 || strlen($form['company']) > 40){
+        $error[] .= 'Företagsnamnet måste vara mellan 2 och 300 tecken.';
+    }
+    if(strlen($form['estimated_time']) < 2 || strlen($form['estimated_time']) > 30){
+        $error[] .= 'Uppskattad tid måste vara mellan 2 och 30 tecken.';
+    }
 
-    if(isset($_POST['tag'])){
-        $tags = $_POST['tag'];
+    if(count($error) > 0){
+        $session->setSession('error',$error);
     }else{
-        $tags = array();
-    }
 
-    if($id > 0){
-        $liaProject->updateProject($project, $tags, $id);
+
+        if(isset($_POST['tag'])){
+            $tags = $_POST['tag'];
+        }else{
+            $tags = array();
+        }
+
+        if($id > 0){
+            $liaProject->updateProject($project, $tags, $id);
+        }
+        else{
+            $liaProject->create($project, $tags);
+        }
+        $session->killSession('error');
+        $session->killSession('projects');
+        redirect($path.'projects/');
     }
-    else{
-        $liaProject->create($project, $tags);
-    }
-    
-    redirect($path.'projects/');
 }
 
 
 require_once('../partials/project-header.php');
 ?>
     <div class="container">
+        <?php
+        //show error if error-session is active
+        if($session->getSession('error')){
+            echo '<div class="alert alert-danger" role="alert">';
+            foreach ($session->getSession('error') as $item) {
+                echo '<li>'.$item.'</li>';
+            }
+            echo '</div>';
+
+            //kill session when 'echoed'.
+            $session->killSession('error');
+        }
+        ?>
     	<form method="POST" action="">
     	  <div class="form-group">
     	    <label for="projectName">Namn</label>
