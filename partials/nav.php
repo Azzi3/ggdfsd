739cdd36c3002d43dba5dde6903f9288d7bf5895
Niklas Petersson <Niklaspetersson@me.com>
2015-02-12 13:33:44 +0100

    Nav updating
    check if student or company owner - profile

--- partials/nav.php
+++ partials/nav.php
@@ -15,15 +15,23 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">    
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="<?php echo $path; ?>student-profile">Profil</a></li>
           <li><a href="<?php echo $path; ?>projects">LIA-Projekt</a></li>
           <li><a href="<?php echo $path; ?>list-courses">LIA-Kurser</a></li>
           <li><a href="<?php echo $path; ?>company-list">Företag</a></li>
           <li><a href="#">Elever</a></li>
+          
           <?php if($signedUser['course_leader'] == 1) : ?>
           <li><a href="<?php echo $path; ?>generate-key">Generera nyckel</a></li>
           <?php endif; ?>
-          <li><a href="<?php echo $path; ?>manage-user">Redigera uppgifter</a></li>
+          <li><a href="<?php echo $path; ?>manage-applications">Mina ansökningar</a></li>
+           
+          <?php if($signedUser['student'] == 1) : ?>
+          <li><a href="<?php echo $path; ?>student-profile">Profil</a></li>
+		  <?php endif; ?>
+		  
+		  <?php if($signedUser['company'] == 1) : ?>
+          <li><a href="<?php echo $path; ?>company_owner">Profil</a></li>
+		  <?php endif; ?>
           <li><a href="<?php echo CURRENT_PATH ?>?logout=1">Logga ut</a></li>
         </ul>
       </div><!-- /.navbar-collapse -->
