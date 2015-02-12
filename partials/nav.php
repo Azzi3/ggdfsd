c27a0dd5e2651be3b60b5b3f67583fb3ec1940d5
Niklas Petersson <Niklaspetersson@me.com>
2015-02-12 15:09:57 +0100

    Nav edit
    Getting the company name / student name on the navbar

--- partials/nav.php
+++ partials/nav.php
@@ -1,3 +1,5 @@
+<?php $company = new Company();?>
+
 <nav class="navbar navbar-default">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -15,17 +17,31 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">    
         <ul class="nav navbar-nav navbar-right">
+        
           <li><a href="<?php echo $path; ?>projects">LIA-Projekt</a></li>
           <li><a href="<?php echo $path; ?>list-courses">LIA-Kurser</a></li>
           <li><a href="<?php echo $path; ?>company-list">Företag</a></li>
           <li><a href="<?php echo $path; ?>list-student">Elever</a></li>
-          
-          <?php if($signedUser['course_leader'] == 1) : ?>
-          <li><a href="<?php echo $path; ?>generate-key">Generera nyckel</a></li>
-          <?php endif; ?>
            
           <li class="dropdown">
-          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Profil <span class="caret"></span></a>
+          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> 
+          	
+          	<?php if($signedUser['student'] == 1) : ?>
+          	<?php echo $signedUser['firstname']?><span class="caret"></span></a>
+          	<?php endif; ?>
+          	
+          	<?php if($signedUser['company_owner'] == 1) : 
+	        if($signedUser['company_id'] != 0){
+		        $companyName = $company->getFromId($signedUser['company_id'])['name'];
+	        }else {
+		        $companyName = 'Inget';
+	        }
+	        echo $companyName;
+			?>
+          	 
+          	 <span class="caret"></span></a>
+          	<?php endif; ?>
+          	
 		  	<ul class="dropdown-menu" role="menu">
           		<?php if($signedUser['student'] == 1) : ?>
           		<li><a href="<?php echo $path; ?>student-profile">Min profil</a></li>
@@ -36,13 +52,17 @@
 		  		<li><a href="<?php echo $path; ?>company-profile?id=<?php $signedUser['company_id'] ?>">Profil</a></li>
 		  		<li><a href="<?php echo $path; ?>manage-applications">Ansökningar</a></li>
 		  		<?php endif; ?>
+		  		
+		  		<?php if($signedUser['course_leader'] == 1) : ?>
+		  		<li><a href="<?php echo $path; ?>generate-key">Generera nyckel</a></li>
+		  		<?php endif; ?>
 		  		<li><a href="<?php echo CURRENT_PATH ?>?logout=1">Logga ut</a></li>
 		  	</ul>
-		  </li> 
+		  </li>		          
 		          
-		</ul>
+		</ul><!-- /.navbar-nav -->
       </div><!-- /.navbar-collapse -->
     <?php endif; ?>
 
-  </div><!-- /.container-fluid -->
+  </div><!-- /.container -->
 </nav>
