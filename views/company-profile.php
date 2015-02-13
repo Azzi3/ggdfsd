4f885813abf02f95cef68011566ede08f58270fe
Bachstatter <joachim.bachstatter@gmail.com>
2015-02-13 09:36:31 +0100

    refactor

--- views/company-profile.php
+++ views/company-profile.php
@@ -1,306 +1,71 @@
-	<!DOCTYPE html>
-	<html lang="en">
-
-	<head>
-
-	    <meta charset="utf-8">
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <meta name="description" content="">
-	    <meta name="author" content="">
-
-	    <title>One Page Wonder - Start Bootstrap Template</title>
-
-	    <!-- Bootstrap Core CSS -->
-	    <link href="css/bootstrap.min.css" rel="stylesheet">
-	    <!-- Custom CSS -->
-
-	    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	    <!--[if lt IE 9]>
-	        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	    <![endif]-->
-
-	</head>
-
-	<body>
-
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">LIA Banken</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="LIA-Projekt.html">LIA-Projekt</a></li>
-                    <li><a href="LIA-Kurser.html">LIA-Kurser</a></li>
-                    <li><a href="Företag.html">Företag</a></li>
-                    <li><a href="Profil.html">Profil</a></li>
-                    <li><a href="Logga-ut.html">Logga ut</a></li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
 
 
-	    <!-- Full Width Image Header -->
-	    <header class="header-color">
-	        <div class="headline">
-	            <div class="row">
-	            	<div class="col-md-3"><img class="img-thumbnail img-responsive pull-center" src="http://placehold.it/200x200"></div>
-	            	<div class="col-md-8 "><h2 class="h2" align="left" >Will Knock Your Socks Off</h2></div>
-	            	<div class="col-md-1 "></div>
-	            </div>
-	        </div>
-	    </header>
-
-
- <!-- left side of page-->
-  <div class="col-sm-3" style="border-right: medium solid;"> 
-	    <h2 class="featurette-heading" >Information</h2>
-	    <h3>Telefonummer:</h3>
-	    <h3>Stad:</h3>
-	    <h3>Email:</h3>
-	    <h3>Hemsida:</h3><br><br><br><br><br><br>
+<!-- Full Width Image Header -->
+<header class="jumbotron">
+	<div class="headline">
+		<div class="row">
+			<div class="col-md-3"><img class="img-thumbnail img-responsive pull-center" src="http://placehold.it/200x200"></div>
+			<div class="col-md-8 "><h2 class="h2" align="left" >Will Knock Your Socks Off</h2></div>
+			<div class="col-md-1 "></div>
+		</div>
 	</div>
+</header>
 
- <!-- center side of page-->
-  <div class="col-sm-9">
-  <h2 class="featurette-heading" align="center">Om</h2>
-  <p class="col-sm-6 lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-  <p class="col-sm-6 lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-  </div>
-
-
-
- <!-- left side of page-->
-   <div class="col-sm-9" align="center" style="border-top: medium solid;">
-   <h2 class="featurette-heading" >Taggar</h2>
-			<h2><span class="label label-primary">HTML5</span>
-  	<span class="label label-primary">CSS3</span>
-  	<span class="label label-primary">PHP</span>
-  	<span class="label label-primary">JACA</span>
-  	<span class="label label-primary">SASS</span></h2>
+<div class="container">
+<!-- left side of page-->
+<div class="col-sm-3" style="border-right: medium solid;"> 
+	<h2 class="featurette-heading" >Information</h2>
+	<h3>Telefonummer:</h3>
+	<h3>Stad:</h3>
+	<h3>Email:</h3>
+	<h3>Hemsida:</h3><br><br><br><br><br><br>
 </div>
 
-    <table class="table">
-      <th>Template Name</th><th align="center">Message </th><th>Pågående projekt</th>
-      <tr><td>Uncompleted Profile A</td>
-      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis gravida metus, sagittis vulputate dolor dictum eget. Quisque nec placerat enim. Ut ac neque magna. Sed quam mauris, consequat a condimentum non, congue ut turpis. Nullam et eleifend ante. Sed molestie eros leo, non condimentum leo condimentum sed. Cras nulla risus, porta lacinia sem non, vestibulum ultricies diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ligula orci, bibendum elementum malesuada vitae, sollicitudin id elit. Etiam eget orci a metus sodales aliquet nec nec massa. Proin posuere porttitor sapien, a venenatis nisi volutpat non. Aliquam erat volutpat. Sed elementum ultricies quam, et tempus neque consequat vitae. Quisque sodales ut ipsum nec rhoncus. Donec ut justo ut tellus sollicitudin dictum. Morbi posuere dapibus placerat. Ut quis ligula a orci efficitur lacinia dignissim in enim. Mauris quis enim mi. Proin luctus hendrerit enim. Mauris bibendum nulla id mauris accumsan lacinia. Etiam quis justo massa.</td>
-      <td><a href="#" class="btn btn-primary" role="button">Ansök</a></td></tr>
-
-      <tr><td><p>Uncompleted Profile A </p></td>
-      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis gravida metus, sagittis vulputate dolor dictum eget. Quisque nec placerat enim. Ut ac neque magna. Sed quam mauris, consequat a condimentum non, congue ut turpis. Nullam et eleifend ante. Sed molestie eros leo, non condimentum leo condimentum sed. Cras nulla risus, porta lacinia sem non, vestibulum ultricies diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ligula orci, bibendum elementum malesuada vitae, sollicitudin id elit. Etiam eget orci a metus sodales aliquet nec nec massa. Proin posuere porttitor sapien, a venenatis nisi volutpat non. Aliquam erat volutpat. Sed elementum ultricies quam, et tempus neque consequat vitae. Quisque sodales ut ipsum nec rhoncus. Donec ut justo ut tellus sollicitudin dictum. Morbi posuere dapibus placerat. Ut quis ligula a orci efficitur lacinia dignissim in enim. Mauris quis enim mi. Proin luctus hendrerit enim. Mauris bibendum nulla id mauris accumsan lacinia. Etiam quis justo massa.</td>
-      <td><a href="#" class="btn btn-primary" role="button">Ansök</a></td></tr>
-
-      <tr><td><p>Uncompleted Profile A </p></td>
-      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis gravida metus, sagittis vulputate dolor dictum eget. Quisque nec placerat enim. Ut ac neque magna. Sed quam mauris, consequat a condimentum non, congue ut turpis. Nullam et eleifend ante. Sed molestie eros leo, non condimentum leo condimentum sed. Cras nulla risus, porta lacinia sem non, vestibulum ultricies diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ligula orci, bibendum elementum malesuada vitae, sollicitudin id elit. Etiam eget orci a metus sodales aliquet nec nec massa. Proin posuere porttitor sapien, a venenatis nisi volutpat non. Aliquam erat volutpat. Sed elementum ultricies quam, et tempus neque consequat vitae. Quisque sodales ut ipsum nec rhoncus. Donec ut justo ut tellus sollicitudin dictum. Morbi posuere dapibus placerat. Ut quis ligula a orci efficitur lacinia dignissim in enim. Mauris quis enim mi. Proin luctus hendrerit enim. Mauris bibendum nulla id mauris accumsan lacinia. Etiam quis justo massa.</td>
-      <td><a href="#" class="lead btn btn-primary" role="button">Ansök</a></td></tr>
-    </table>
+<!-- center side of page-->
+<div class="col-sm-9">
+	<h2 class="text-center">Om</h2>
+	<p class="col-sm-6 lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+	<p class="col-sm-6 lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
 
 
-<div class="form-group">
-	<div class="col-sm-12 panel-footer clearfix">
-		<div class="col-sm-11">
-		<input type="text" class="form-control" id="Komenterar" placeholder="text">
+
+<!-- left side of page-->
+<div class="col-sm-9" style="border-top: medium solid;">
+	<h2 class="featurette-heading" >Taggar</h2>
+	<h2><span class="label label-primary">HTML5</span>
+		<span class="label label-primary">CSS3</span>
+		<span class="label label-primary">PHP</span>
+		<span class="label label-primary">JACA</span>
+		<span class="label label-primary">SASS</span></h2>
 	</div>
-		<div class="col-sm-1">
-    <a href="#" class="btn btn-primary">Komenterar</a></div>
-	</div>
-    </div>
 
+	<table class="table">
+		<th>Template Name</th><th align="center">Message </th><th>Pågående projekt</th>
+		<tr>
+			<td>Uncompleted Profile A</td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis gravida metus, sagittis vulputate dolor dictum eget. Quisque nec placerat enim. Ut ac neque magna. Sed quam mauris, consequat a condimentum non, congue ut turpis. Nullam et eleifend ante. Sed molestie eros leo, non condimentum leo condimentum sed. Cras nulla risus, porta lacinia sem non, vestibulum ultricies diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ligula orci, bibendum elementum malesuada vitae, sollicitudin id elit. Etiam eget orci a metus sodales aliquet nec nec massa. Proin posuere porttitor sapien, a venenatis nisi volutpat non. Aliquam erat volutpat. Sed elementum ultricies quam, et tempus neque consequat vitae. Quisque sodales ut ipsum nec rhoncus. Donec ut justo ut tellus sollicitudin dictum. Morbi posuere dapibus placerat. Ut quis ligula a orci efficitur lacinia dignissim in enim. Mauris quis enim mi. Proin luctus hendrerit enim. Mauris bibendum nulla id mauris accumsan lacinia. Etiam quis justo massa.</td>
+			<td><a href="#" class="btn btn-primary" role="button">Ansök</a></td>
+		</tr>
 
-
-	</body>
-	<style>
-
-
-
-	body {
-	    margin-top: 50px; /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
-	}
-
-
-	.header-color {
-	    display: block;
-	    width: 100%;
-	    text-align: center;
-	    background-color: #A9A9A9;
-	  
-	}
-	.h2 {
-		color: #ffffff;
-		padding-top: 60px;
-
-	}
-
-	.headline {
-	    padding: 120px 0;
-	    text-align: center;
-	}
-
-	.headline h1 {
-	    font-size: 130px;
-	    background: #fff;
-	    background: rgba(255,255,255,0.9);
-	}
-
-	.featurette-divider {
-	    margin: 80px 0;
-	}
-
-	.featurette {
-	    overflow: hidden;
-	}
-
-	.featurette-image.pull-left {
-	    margin-right: 40px;
-	}
-
-	.featurette-image.pull-right {
-	    margin-left: 40px;
-	}
-
-	.featurette-heading {
-	    font-size: 50px;
-	}
-
-	footer {
-	    margin: 50px 0;
-	}
-
-	@media(max-width:1200px) {
-	    .headline h1 {
-	        font-size: 140px;
-	    }
-
-	    .headline h2 {
-	        font-size: 63px;
-	    }
-
-	    .featurette-divider {
-	        margin: 50px 0;
-	    }
-
-	    .featurette-image.pull-left {
-	        margin-right: 20px;
-	    }
-
-	    .featurette-image.pull-right {
-	        margin-left: 20px;
-	    }
-
-	    .featurette-heading {
-	        font-size: 35px;
-	    }
-	}
-
-	@media(max-width:991px) {
-	    .headline h1 {
-	        font-size: 105px;
-	    }
-
-	    .headline h2 {
-	        font-size: 50px;
-	    }
-
-	    .featurette-divider {
-	        margin: 40px 0;
-	    }
-
-	    .featurette-image {
-	        max-width: 50%;
-	    }
-
-	    .featurette-image.pull-left {
-	        margin-right: 10px;
-	    }
-
-	    .featurette-image.pull-right {
-	        margin-left: 10px;
-	    }
-
-	    .featurette-heading {
-	        font-size: 30px;
-	    }
-	}
-
-	@media(max-width:768px) {
-	    .container {
-	        margin: 0 15px;
-	    }
-
-	    .featurette-divider {
-	        margin: 40px 0;
-	    }
-
-	    .featurette-heading {
-	        font-size: 25px;
-	    }
-	}
-
-	@media(max-width:668px) {
-	    .headline h1 {
-	        font-size: 70px;
-	    }
-
-	    .headline h2 {
-	        font-size: 32px;
-	    }
-
-	    .featurette-divider {
-	        margin: 30px 0;
-	    }
-	}
-
-	@media(max-width:640px) {
-	    .headline {
-	        padding: 75px 0 25px 0;
-	    }
-
-	    .headline h1 {
-	        font-size: 60px;
-	    }
-
-	    .headline h2 {
-	        font-size: 30px;
-	    }
-	}
-
-	@media(max-width:375px) {
-	    .featurette-divider {
-	        margin: 10px 0;
-	    }
-
-	    .featurette-image {
-	        max-width: 100%;
-	    }
-
-	    .featurette-image.pull-left {
-	        margin-right: 0;
-	        margin-bottom: 10px;
-	    }
-
-	    .featurette-image.pull-right {
-	        margin-bottom: 10px;
-	        margin-left: 0;
-	    }
-	}
-
-
-	</style>
-	</html>
+		<tr>
+			<td><p>Uncompleted Profile A </p></td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis gravida metus, sagittis vulputate dolor dictum eget. Quisque nec placerat enim. Ut ac neque magna. Sed quam mauris, consequat a condimentum non, congue ut turpis. Nullam et eleifend ante. Sed molestie eros leo, non condimentum leo condimentum sed. Cras nulla risus, porta lacinia sem non, vestibulum ultricies diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ligula orci, bibendum elementum malesuada vitae, sollicitudin id elit. Etiam eget orci a metus sodales aliquet nec nec massa. Proin posuere porttitor sapien, a venenatis nisi volutpat non. Aliquam erat volutpat. Sed elementum ultricies quam, et tempus neque consequat vitae. Quisque sodales ut ipsum nec rhoncus. Donec ut justo ut tellus sollicitudin dictum. Morbi posuere dapibus placerat. Ut quis ligula a orci efficitur lacinia dignissim in enim. Mauris quis enim mi. Proin luctus hendrerit enim. Mauris bibendum nulla id mauris accumsan lacinia. Etiam quis justo massa.</td>
+			<td><a href="#" class="btn btn-primary" role="button">Ansök</a></td>
+		</tr>
+		<tr>
+			<td><p>Uncompleted Profile A </p></td>
+			<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis gravida metus, sagittis vulputate dolor dictum eget. Quisque nec placerat enim. Ut ac neque magna. Sed quam mauris, consequat a condimentum non, congue ut turpis. Nullam et eleifend ante. Sed molestie eros leo, non condimentum leo condimentum sed. Cras nulla risus, porta lacinia sem non, vestibulum ultricies diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ligula orci, bibendum elementum malesuada vitae, sollicitudin id elit. Etiam eget orci a metus sodales aliquet nec nec massa. Proin posuere porttitor sapien, a venenatis nisi volutpat non. Aliquam erat volutpat. Sed elementum ultricies quam, et tempus neque consequat vitae. Quisque sodales ut ipsum nec rhoncus. Donec ut justo ut tellus sollicitudin dictum. Morbi posuere dapibus placerat. Ut quis ligula a orci efficitur lacinia dignissim in enim. Mauris quis enim mi. Proin luctus hendrerit enim. Mauris bibendum nulla id mauris accumsan lacinia. Etiam quis justo massa.</td>
+			<td><a href="#" class="lead btn btn-primary" role="button">Ansök</a></td>
+		</tr>
+	</table>
+	<form class="form-horizontal">
+		<div class="form-group">
+			<div class="col-sm-10">
+				<input type="text" class="form-control " id="Komenterar" placeholder="text">
+			</div>
+		</div>
+		<a href="#" class="btn btn-primary">Komenterar</a>
+	</form>
+</div>
