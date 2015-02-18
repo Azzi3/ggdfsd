6136ae9fb65710a7951eff1430b08b7ed34745ce
Timalex <tim.alexander.gunnarsson@gmail.com>
2015-02-18 11:04:41 +0100

    Tog bort redundant navigerings lista (De finns i en riktig meny nu)

--- views/main.php
+++ views/main.php
@@ -1,9 +1,1 @@
-<ul>
-	<li><a href="<?php echo $path; ?>projects">Projekt</a></li>
-	<li><a href="<?php echo $path; ?>company-list">Företag</a></li>
-  <li><a href="<?php echo $path; ?>list-courses">Kurser</a></li>
-  <li><a href="<?php echo $path; ?>list-student">Studenter</a></li>
-  <li><a href="<?php echo $path; ?>manage-applications">Mina ansökningar</a></li>
-</ul>
-
 <?php require_once('login.php'); ?>
