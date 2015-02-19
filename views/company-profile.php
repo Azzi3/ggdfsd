02ebedaaf8c0a29e2451f51c955dd67a9d8b12f5
MrMagnus <magnus_karlsson@live.com>
2015-02-19 13:51:58 +0100

    I dont even

--- views/company-profile.php
+++ views/company-profile.php
@@ -9,8 +9,8 @@
     $userInfo = $signedUser;
   }
   
-  $companyName = $company->getFromId($signedUser['company_id'])['name'];
-  $companyProfileInfo = $company->getFromId($userInfo['id']);
+  $companyInfo = $company->getFromId($signedUser['company_id']);
+  $companyProfileInfo = $company->getFromId($userInfo['company_id']);
   $userCounty = $county->getMunicipalityName($userInfo['municipality']);
   $companyTags = $company->getTags($signedUser['company_id']);
   $companyProjects = $company->getProjects($signedUser['company_id']);
@@ -21,17 +21,23 @@
     <div class="jumbotron">
 
       <?php if($session->getSession('guid') == $userInfo['guid']) : ?>
-        <a class="edit-anchor" href="<?php echo $path . "manage-company?id=" . $companyProfileInfo['id']; ?>"> <button class="btn pull-right">Redigera uppgifter</button></a>
+        <a class="edit-anchor" href="<?php echo $path . "manage-company?id=" . $companyInfo['id']; ?>"> <button class="btn pull-right">Redigera uppgifter</button></a>
       <?php endif; ?>
 
       <div class="row">
         <div class="col-md-3 text-center">
           
-          <a href="<?php echo $path . "images/users/" . $userInfo['id'] . "/" . $companyProfileInfo['picture']; ?>" target="_blank">
-            <img class="profile-img" src=" <?php echo $path . "images/users/" . $userInfo['id'] . "/tum_" . $companyProfileInfo['picture']; ?> " alt="">
-          </a>
-          
-          <h3 class="text-center"> <?php echo $companyName; ?> </h3>
+
+
+          <?php if($companyInfo['image']): ?>
+						<a href="<?php echo $path . "images/users/" . $userInfo['name'] . "/" . $companyProfileInfo['image']; ?>" target="_blank">
+            	<img class="profile-img" src="<?php echo $path . "images/company/" . $companyInfo['name'] . "/" . $companyInfo['image']; ?>" alt="">
+          	</a>
+          <?php else: ?>
+						<img class="profile-img" src="<?php echo $path . "images/placeholder.jpg" ?>" alt="">
+          <?php endif; ?>
+			
+          <h3 class="text-center"> <?php echo $companyInfo['name']; ?> </h3>
           <h4><?php echo $userCounty['name']; ?></h4>
         </div>
       </div>
@@ -44,7 +50,7 @@
       <h3 class="text-center">Info</h3>  
       
       <ul>
-		<!-- <li>Telefon: <?php echo $companyProfileInfo['phone']; ?> </li> -->
+				<li>Telefon: <?php echo $companyProfileInfo['phone']; ?> </li> 
         <li>Email: <?php echo $userInfo['email']; ?> </li>
         <li>Hemsida: <?php echo $companyProfileInfo['website_url']; ?> </li>
       </ul>
@@ -78,12 +84,17 @@
   </div>
   
   <div class="row">
-  	<table>
+  	<div class="col-md-12"><h2 class="text-center">VÅRA PROJECT</h2></div>
+  </div>
+
+
+  <div class="row">
+  	<table class="table table-striped table-hover">
 	  	<thead>
 	  		<tr>
   				<th>Namn</th>
   				<th>Beskrivning</th>
-  				<th><h2>VÅRA PROJEKT</h2></th>
+  				<th></th>
   			</tr>	
 	  	</thead>
   		<tbody>
@@ -94,7 +105,8 @@
 						<td><?php echo $project['description']; ?></td>
 						<td>
 							<?php if($signedUser['company_owner'] == 1 && $signedUser['company_id'] == $project['company_id']) : ?>
-								<a href="<?php echo $path; ?>manage-projects?id=<?php echo $project['id']; ?>"><button class="btn">Ändra</button></a>
+								<a class="push-right" href="<?php echo $path; ?>manage-projects?id=<?php echo $project['id']; ?>"><button class="btn">Ändra</button></a>
+								<a class="btn btn-danger" id="deleteProjectBtn" data-projectid="<?php echo $project['id']; ?>" data-toggle="modal" data-target="#deleteModal" >Ta bort</a>
 							<?php endif; ?>
 						</td>
 					</tr>	  
