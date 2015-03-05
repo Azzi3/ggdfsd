623de8b45cc0d7b1e7287ed29adbed7f168b036e
Bachstatter <joachim.bachstatter@gmail.com>
2015-03-05 15:22:05 +0100

    new btn projets

--- views/projects.php
+++ views/projects.php
@@ -71,8 +71,8 @@ if(isset($_GET['deleteid'])){
 						} ?></td>
 						<td>
 							<?php if($signedUser['company_owner'] == 1 && $signedUser['company_id'] == $project['company_id']) : ?>
-								<a href="<?php echo $path; ?>manage-projects?id=<?php echo $project['id']; ?>"><button class="btn">Ändra</button></a>
-								<a id="deleteProjectBtn" data-name="<?php echo $project['name'];  ?>" data-projectid="<?php echo $project['id']; ?>" class="btn" data-toggle="modal" data-target="#deleteModal" >Ta bort</a>
+								<a class="btn btn-primary" href="<?php echo $path; ?>manage-projects?id=<?php echo $project['id']; ?>">Ändra</a>
+								<a class="btn btn-danger" id="deleteProjectBtn" data-name="<?php echo $project['name'];  ?>" data-projectid="<?php echo $project['id']; ?>" class="btn" data-toggle="modal" data-target="#deleteModal" >Ta bort</a>
 							<?php endif; ?>
 
 						</td>
