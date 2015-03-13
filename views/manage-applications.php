f29270a0f3a0da7c111c69c8f13ed63f252e2717
Bachstatter <joachim.bachstatter@gmail.com>
2015-03-13 14:13:40 +0100

    no  application state

--- views/manage-applications.php
+++ views/manage-applications.php
@@ -93,72 +93,81 @@ if(isset($_POST['report']) && isset($_GET['app']) && isset($_GET['id'])){
 </div>
 
 <div class="container">
-	<div class="table-responsive">
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th><?php echo $thName; ?></th>
-					<th>Lia kurs</th>
-					<th>Status</th>
-					<th>Ansökning slutar</th>
-					<th></th>
-				</tr>
-			</thead>
+		<?php if(count($myApplications) == 0){
+			echo '<h3 class="text-center">Inga ansökningar</h3>';
+			}else{ ?>
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th><?php echo $thName; ?></th>
+						<th>Lia kurs</th>
+						<th>Status</th>
+						<th>Ansökning slutar</th>
+						<th></th>
+					</tr>
+				</thead>
 
-			<tbody>
-			<?php
-			foreach ($myApplications as $ApplicationForm){
+				<tbody>
+				<?php
+				foreach ($myApplications as $ApplicationForm){
 
-						$acceptButton = '';
-					 	$denyButton = '';
-					 	$finishButton = '';
-					 	$formButton = '';
-
-
-				if($signedUser['student']){				
-						$company = $CompanyObj->getFromId($ApplicationForm['company_id']); 
-						$tdName = $company['name'];
-						$deleteBtn = 	'<a data-name=" ansökan till '.$tdName.'" data-applicationid="' . $ApplicationForm['id'] . '"  id="deleteApplicationBtn" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">Ta bort</a>';
-				}elseif($signedUser['company_owner']){
-						$deleteBtn = '';
-					 	$user = $UserObj->getUserByid($ApplicationForm['user_id']);
-					 	$tdName = $user['firstname'] . ' ' . $user['lastname'];
-
-					 	$acceptButton = '<a href="'.CURRENT_PATH.'?accept='.$ApplicationForm['id'].'&uid='.$user['id'].'" class="btn btn-success">Godkänd</a>';
-					 	$denyButton = '<a href="'.CURRENT_PATH.'?deny='.$ApplicationForm['id'].'&uid='.$user['id'].'" class="btn btn-danger">Neka</a>';
-					 	$finishButton = '<a href="'.CURRENT_PATH.'?finish='.$ApplicationForm['id' ].'&uid='.$user['id'].'" class="btn btn-warning">Genomförd</a>';
-					 	$formButton = '<form action="?app='.$ApplicationForm['user_id'].'&id='.$ApplicationForm['id'].'" method="POST">
-					<textarea name="report" class="form-control" placeholder="Omdömme">'.$ApplicationForm['report'].'</textarea><br>
-					<button class="btn btn-default" type="submit">Spara omdömme</button>
-					</form>';
-				}
-
-				$courseName = $course->getFromId($ApplicationForm['course_id'])['name'];
-				$lastDay = $course->getFromId($ApplicationForm['course_id'])['course_start'];
+							$acceptButton = '';
+						 	$denyButton = '';
+						 	$finishButton = '';
+						 	$formButton = '';
 
 
-				if($ApplicationForm['status'] == 0){
-					$status = 'Ny ansökan';
-					$btn = $acceptButton.' '.$denyButton;
-				}else if($ApplicationForm['status'] == 1){
-					$status = 'Godkänd';
-					$btn = $denyButton.' '.$finishButton;
-				}else if($ApplicationForm['status'] == 2){
-					$status = 'Nekad';
-					$btn = $acceptButton;
-				}else if($ApplicationForm['status'] == 3){
-					$status = 'Genomförd';
-					$btn = $formButton;
-				}
+					if($signedUser['student']){				
+							$company = $CompanyObj->getFromId($ApplicationForm['company_id']); 
+							$tdName = $company['name'];
+							$deleteBtn = 	'<a data-name=" ansökan till '.$tdName.'" data-applicationid="' . $ApplicationForm['id'] . '"  id="deleteApplicationBtn" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">Ta bort</a>';
+					}elseif($signedUser['company_owner']){
+							$deleteBtn = '';
+						 	$user = $UserObj->getUserByid($ApplicationForm['user_id']);
+						 	$tdName = $user['firstname'] . ' ' . $user['lastname'];
 
-			?>
-			
-				 <tr>
-				 	<td><?php echo $tdName; ?></td>
-				 	<td><?php echo $courseName; ?></td>
-				 	<td><b><?php echo $status; ?></b></td>
-				 	<td><?php echo $lastDay; ?></td>
-				 	<td><?php echo $btn.$deleteBtn; ?></td>
-				 </tr>
-			 	 <?php } ?>
+						 	$acceptButton = '<a href="'.CURRENT_PATH.'?accept='.$ApplicationForm['id'].'&uid='.$user['id'].'" class="btn btn-success">Godkänd</a>';
+						 	$denyButton = '<a href="'.CURRENT_PATH.'?deny='.$ApplicationForm['id'].'&uid='.$user['id'].'" class="btn btn-danger">Neka</a>';
+						 	$finishButton = '<a href="'.CURRENT_PATH.'?finish='.$ApplicationForm['id' ].'&uid='.$user['id'].'" class="btn btn-warning">Genomförd</a>';
+						 	$formButton = '<form action="?app='.$ApplicationForm['user_id'].'&id='.$ApplicationForm['id'].'" method="POST">
+						<textarea name="report" class="form-control" placeholder="Omdömme">'.$ApplicationForm['report'].'</textarea><br>
+						<button class="btn btn-default" type="submit">Spara omdömme</button>
+						</form>';
+					}
+
+					$courseName = $course->getFromId($ApplicationForm['course_id'])['name'];
+					$lastDay = $course->getFromId($ApplicationForm['course_id'])['course_start'];
+
+
+					if($ApplicationForm['status'] == 0){
+						$status = 'Ny ansökan';
+						$btn = $acceptButton.' '.$denyButton;
+					}else if($ApplicationForm['status'] == 1){
+						$status = 'Godkänd';
+						$btn = $denyButton.' '.$finishButton;
+					}else if($ApplicationForm['status'] == 2){
+						$status = 'Nekad';
+						$btn = $acceptButton;
+					}else if($ApplicationForm['status'] == 3){
+						$status = 'Genomförd';
+						$btn = $formButton;
+					}
+
+				?>
+				
+					 <tr>
+					 	<td><?php echo $tdName; ?></td>
+					 	<td><?php echo $courseName; ?></td>
+					 	<td><b><?php echo $status; ?></b></td>
+					 	<td><?php echo $lastDay; ?></td>
+					 	<td><?php echo $btn.$deleteBtn; ?></td>
+					 </tr>
+				 	 <?php } ?>
+
+			</tbody>
+		</table>
+		<?php } ?>
+	</div>
+</body>
+</html>
 
